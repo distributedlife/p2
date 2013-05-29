f37853812f301e3d3991f0aed188406a0ecce02f
Andrew Carr <acarr@thoughtworks.com>
2013-05-29 19:58:17 +1000

    adding mixture project into repo

--- mixture_project/compass/config.rb
+++ mixture_project/compass/config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 # -----------------------------------------------------------------------------
-project_path = "/Users/acarr/Dropbox/Code/workspace/magazine_mix/"
+project_path = "/Users/acarr/Dropbox/Code/workspace/magazine_style/mixture_project/"
 
 
 # Set this to the root of your project when deployed:
