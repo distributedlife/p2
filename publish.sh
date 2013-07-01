acbc70204b1429c504d1f133a295ef636e985e6b
Ryan Boucher <ryan.boucher@distributedlife.com>
2013-07-01 15:04:36 +1000

    changed to origin from distributedlife

--- publish.sh
+++ publish.sh
@@ -25,7 +25,7 @@ git add .
 git commit -m "$1"
 
 echo "Pushing changes to gh-pages ($1)"
-git push distributedlife gh-pages
+git push origin gh-pages
 [ "$(echo $?)" != "0" ] && echo "Error pushing changes. We're going to stop here while you resolve it." && exit 1
 
 git checkout "source"
